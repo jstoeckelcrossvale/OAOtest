55dc796078c6606ddfcfc8e14a1fa6eacd4157f4
bpmsAdmin <>
2018-04-05 11:28:41 -0500

    Created PhoneNr data objects {/CardDispute/src/main/java/com/crossvale/carddispute/PhoneNr.java}

--- CardDispute/src/main/java/com/crossvale/carddispute/PhoneNr.java
+++ CardDispute/src/main/java/com/crossvale/carddispute/PhoneNr.java
@@ -10,8 +10,39 @@ public class PhoneNr implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label(value = "phoneType")
+   private java.lang.String phoneType;
+   @org.kie.api.definition.type.Label(value = "number")
+   private java.lang.String number;
+
    public PhoneNr()
    {
+   }
+
+   public java.lang.String getPhoneType()
+   {
+      return this.phoneType;
+   }
+
+   public void setPhoneType(java.lang.String phoneType)
+   {
+      this.phoneType = phoneType;
+   }
+
+   public java.lang.String getNumber()
+   {
+      return this.number;
+   }
+
+   public void setNumber(java.lang.String number)
+   {
+      this.number = number;
+   }
+
+   public PhoneNr(java.lang.String phoneType, java.lang.String number)
+   {
+      this.phoneType = phoneType;
+      this.number = number;
    }
 
 }
