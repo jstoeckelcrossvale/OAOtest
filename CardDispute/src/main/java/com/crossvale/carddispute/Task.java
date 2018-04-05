1f51ff4421780d93155dfc26b712a2008c859a6d
bpmsAdmin <>
2018-04-05 14:38:34 -0500

    Added fields to Task object {/CardDispute/src/main/java/com/crossvale/carddispute/Task.java}

--- CardDispute/src/main/java/com/crossvale/carddispute/Task.java
+++ CardDispute/src/main/java/com/crossvale/carddispute/Task.java
@@ -10,8 +10,107 @@ public class Task implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Label(value = "taskNumber")
+   private java.lang.String taskNumber;
+   @org.kie.api.definition.type.Label(value = "taskType")
+   private java.lang.String taskType;
+   @org.kie.api.definition.type.Label(value = "description")
+   private java.lang.String description;
+   @org.kie.api.definition.type.Label(value = "ticketNumber")
+   private java.lang.String ticketNumber;
+   @org.kie.api.definition.type.Label(value = "customInfo")
+   private java.lang.String customInfo;
+   @org.kie.api.definition.type.Label(value = "dueDate")
+   private java.util.Date dueDate;
+   @org.kie.api.definition.type.Label(value = "userName")
+   private java.lang.String userName;
+
    public Task()
    {
+   }
+
+   public java.lang.String getTaskNumber()
+   {
+      return this.taskNumber;
+   }
+
+   public void setTaskNumber(java.lang.String taskNumber)
+   {
+      this.taskNumber = taskNumber;
+   }
+
+   public java.lang.String getTaskType()
+   {
+      return this.taskType;
+   }
+
+   public void setTaskType(java.lang.String taskType)
+   {
+      this.taskType = taskType;
+   }
+
+   public java.lang.String getDescription()
+   {
+      return this.description;
+   }
+
+   public void setDescription(java.lang.String description)
+   {
+      this.description = description;
+   }
+
+   public java.lang.String getTicketNumber()
+   {
+      return this.ticketNumber;
+   }
+
+   public void setTicketNumber(java.lang.String ticketNumber)
+   {
+      this.ticketNumber = ticketNumber;
+   }
+
+   public java.lang.String getCustomInfo()
+   {
+      return this.customInfo;
+   }
+
+   public void setCustomInfo(java.lang.String customInfo)
+   {
+      this.customInfo = customInfo;
+   }
+
+   public java.util.Date getDueDate()
+   {
+      return this.dueDate;
+   }
+
+   public void setDueDate(java.util.Date dueDate)
+   {
+      this.dueDate = dueDate;
+   }
+
+   public java.lang.String getUserName()
+   {
+      return this.userName;
+   }
+
+   public void setUserName(java.lang.String userName)
+   {
+      this.userName = userName;
+   }
+
+   public Task(java.lang.String taskNumber, java.lang.String taskType,
+         java.lang.String description, java.lang.String ticketNumber,
+         java.lang.String customInfo, java.util.Date dueDate,
+         java.lang.String userName)
+   {
+      this.taskNumber = taskNumber;
+      this.taskType = taskType;
+      this.description = description;
+      this.ticketNumber = ticketNumber;
+      this.customInfo = customInfo;
+      this.dueDate = dueDate;
+      this.userName = userName;
    }
 
 }
