0601e3ad81b10e1658236917e6afe394223412a1
bpmsAdmin <>
2018-04-17 10:58:04 -0500

    Updated Customer object {/CardDispute/src/main/java/com/crossvale/carddispute/Customer.java}

--- CardDispute/src/main/java/com/crossvale/carddispute/Customer.java
+++ CardDispute/src/main/java/com/crossvale/carddispute/Customer.java
@@ -23,9 +23,6 @@ public class Customer implements java.io.Serializable
    @org.kie.api.definition.type.Label("expirationDate")
    private Date expirationDate;
 
-   @org.kie.api.definition.type.Label("address")
-   private com.crossvale.carddispute.Address address;
-
    @org.kie.api.definition.type.Label("nationalityCode")
    private java.lang.String nationalityCode;
 
@@ -35,14 +32,50 @@ public class Customer implements java.io.Serializable
    @org.kie.api.definition.type.Label("email")
    private java.lang.String email;
 
-   @org.kie.api.definition.type.Label("phoneNr")
-   private com.crossvale.carddispute.PhoneNr phoneNr;
-
    @org.kie.api.definition.type.Label("customerNumber")
    private java.lang.String customerNumber;
 
-   @org.kie.api.definition.type.Label(value = "customerName")
+   @org.kie.api.definition.type.Label("customerName")
    private java.lang.String customerName;
+
+   @org.kie.api.definition.type.Label(value = "customerType")
+   private java.lang.String customerType;
+
+   @org.kie.api.definition.type.Label(value = "ssn")
+   private java.lang.String ssn;
+
+   @org.kie.api.definition.type.Label(value = "primaryPhoneNumber")
+   private java.lang.String primaryPhoneNumber;
+
+   @org.kie.api.definition.type.Label(value = "alternatePhoneNumber")
+   private java.lang.String alternatePhoneNumber;
+
+   @org.kie.api.definition.type.Label(value = "address1")
+   private java.lang.String address1;
+
+   @org.kie.api.definition.type.Label(value = "address2")
+   private java.lang.String address2;
+
+   @org.kie.api.definition.type.Label(value = "city")
+   private java.lang.String city;
+
+   @org.kie.api.definition.type.Label(value = "state")
+   private java.lang.String state;
+
+   @org.kie.api.definition.type.Label(value = "postalCode")
+   private java.lang.String postalCode;
+
+   @org.kie.api.definition.type.Label(value = "countryCode")
+   private java.lang.String countryCode;
+
+   @org.kie.api.definition.type.Label(value = "countyName")
+   private java.lang.String countyName;
+
+   @org.kie.api.definition.type.Label(value = "primaryEmail")
+   private java.lang.String primaryEmail;
+
+   @org.kie.api.definition.type.Label(value = "alternateEmail")
+   private java.lang.String alternateEmail;
 
    public Customer()
    {
@@ -78,16 +111,6 @@ public class Customer implements java.io.Serializable
       this.issuePlace = issuePlace;
    }
 
-   public com.crossvale.carddispute.Address getAddress()
-   {
-      return this.address;
-   }
-
-   public void setAddress(com.crossvale.carddispute.Address address)
-   {
-      this.address = address;
-   }
-
    public java.lang.String getNationalityCode()
    {
       return this.nationalityCode;
@@ -106,16 +129,6 @@ public class Customer implements java.io.Serializable
    public void setEmail(java.lang.String email)
    {
       this.email = email;
-   }
-
-   public com.crossvale.carddispute.PhoneNr getPhoneNr()
-   {
-      return this.phoneNr;
-   }
-
-   public void setPhoneNr(com.crossvale.carddispute.PhoneNr phoneNr)
-   {
-      this.phoneNr = phoneNr;
    }
 
    public java.util.Date getIssueDate()
@@ -168,26 +181,172 @@ public class Customer implements java.io.Serializable
       this.customerName = customerName;
    }
 
+   public java.lang.String getCustomerType()
+   {
+      return this.customerType;
+   }
+
+   public void setCustomerType(java.lang.String customerType)
+   {
+      this.customerType = customerType;
+   }
+
+   public java.lang.String getSsn()
+   {
+      return this.ssn;
+   }
+
+   public void setSsn(java.lang.String ssn)
+   {
+      this.ssn = ssn;
+   }
+
+   public java.lang.String getPrimaryPhoneNumber()
+   {
+      return this.primaryPhoneNumber;
+   }
+
+   public void setPrimaryPhoneNumber(java.lang.String primaryPhoneNumber)
+   {
+      this.primaryPhoneNumber = primaryPhoneNumber;
+   }
+
+   public java.lang.String getAlternatePhoneNumber()
+   {
+      return this.alternatePhoneNumber;
+   }
+
+   public void setAlternatePhoneNumber(java.lang.String alternatePhoneNumber)
+   {
+      this.alternatePhoneNumber = alternatePhoneNumber;
+   }
+
+   public java.lang.String getAddress1()
+   {
+      return this.address1;
+   }
+
+   public void setAddress1(java.lang.String address1)
+   {
+      this.address1 = address1;
+   }
+
+   public java.lang.String getAddress2()
+   {
+      return this.address2;
+   }
+
+   public void setAddress2(java.lang.String address2)
+   {
+      this.address2 = address2;
+   }
+
+   public java.lang.String getCity()
+   {
+      return this.city;
+   }
+
+   public void setCity(java.lang.String city)
+   {
+      this.city = city;
+   }
+
+   public java.lang.String getState()
+   {
+      return this.state;
+   }
+
+   public void setState(java.lang.String state)
+   {
+      this.state = state;
+   }
+
+   public java.lang.String getPostalCode()
+   {
+      return this.postalCode;
+   }
+
+   public void setPostalCode(java.lang.String postalCode)
+   {
+      this.postalCode = postalCode;
+   }
+
+   public java.lang.String getCountryCode()
+   {
+      return this.countryCode;
+   }
+
+   public void setCountryCode(java.lang.String countryCode)
+   {
+      this.countryCode = countryCode;
+   }
+
+   public java.lang.String getCountyName()
+   {
+      return this.countyName;
+   }
+
+   public void setCountyName(java.lang.String countyName)
+   {
+      this.countyName = countyName;
+   }
+
+   public java.lang.String getPrimaryEmail()
+   {
+      return this.primaryEmail;
+   }
+
+   public void setPrimaryEmail(java.lang.String primaryEmail)
+   {
+      this.primaryEmail = primaryEmail;
+   }
+
+   public java.lang.String getAlternateEmail()
+   {
+      return this.alternateEmail;
+   }
+
+   public void setAlternateEmail(java.lang.String alternateEmail)
+   {
+      this.alternateEmail = alternateEmail;
+   }
+
    public Customer(java.lang.String status, java.lang.String id,
          java.lang.String issuePlace, java.util.Date issueDate,
-         java.util.Date expirationDate,
-         com.crossvale.carddispute.Address address,
-         java.lang.String nationalityCode, java.util.Date birthDate,
-         java.lang.String email, com.crossvale.carddispute.PhoneNr phoneNr,
-         java.lang.String customerNumber, java.lang.String customerName)
+         java.util.Date expirationDate, java.lang.String nationalityCode,
+         java.util.Date birthDate, java.lang.String email,
+         java.lang.String customerNumber, java.lang.String customerName,
+         java.lang.String customerType, java.lang.String ssn,
+         java.lang.String primaryPhoneNumber,
+         java.lang.String alternatePhoneNumber, java.lang.String address1,
+         java.lang.String address2, java.lang.String city,
+         java.lang.String state, java.lang.String postalCode,
+         java.lang.String countryCode, java.lang.String countyName,
+         java.lang.String primaryEmail, java.lang.String alternateEmail)
    {
       this.status = status;
       this.id = id;
       this.issuePlace = issuePlace;
       this.issueDate = issueDate;
       this.expirationDate = expirationDate;
-      this.address = address;
       this.nationalityCode = nationalityCode;
       this.birthDate = birthDate;
       this.email = email;
-      this.phoneNr = phoneNr;
       this.customerNumber = customerNumber;
       this.customerName = customerName;
+      this.customerType = customerType;
+      this.ssn = ssn;
+      this.primaryPhoneNumber = primaryPhoneNumber;
+      this.alternatePhoneNumber = alternatePhoneNumber;
+      this.address1 = address1;
+      this.address2 = address2;
+      this.city = city;
+      this.state = state;
+      this.postalCode = postalCode;
+      this.countryCode = countryCode;
+      this.countyName = countyName;
+      this.primaryEmail = primaryEmail;
+      this.alternateEmail = alternateEmail;
    }
 
 }
