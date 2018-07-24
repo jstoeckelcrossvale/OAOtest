2a2ef80f685d1e887c426043db4af2d74f4a5000
kieserver <>
2018-07-24 10:49:25 -0500

    {/OAO/src/main/java/com/crossvale/oao/Account.java}

--- OAO/src/main/java/com/crossvale/oao/Account.java
+++ OAO/src/main/java/com/crossvale/oao/Account.java
@@ -19,6 +19,15 @@ public class Account implements java.io.Serializable
    @org.kie.api.definition.type.Label("type")
    private java.lang.String type;
 
+   @org.kie.api.definition.type.Label(value = "balance")
+   private com.crossvale.oao.Balance balance;
+
+   @org.kie.api.definition.type.Label(value = "branch_id")
+   private java.lang.String branch_id;
+
+   @org.kie.api.definition.type.Label(value = "account_routing")
+   private com.crossvale.oao.Account_routing account_routing;
+
    public Account()
    {
    }
@@ -53,12 +62,47 @@ public class Account implements java.io.Serializable
       this.type = type;
    }
 
+   public com.crossvale.oao.Balance getBalance()
+   {
+      return this.balance;
+   }
+
+   public void setBalance(com.crossvale.oao.Balance balance)
+   {
+      this.balance = balance;
+   }
+
+   public java.lang.String getBranch_id()
+   {
+      return this.branch_id;
+   }
+
+   public void setBranch_id(java.lang.String branch_id)
+   {
+      this.branch_id = branch_id;
+   }
+
+   public com.crossvale.oao.Account_routing getAccount_routing()
+   {
+      return this.account_routing;
+   }
+
+   public void setAccount_routing(com.crossvale.oao.Account_routing account_routing)
+   {
+      this.account_routing = account_routing;
+   }
+
    public Account(java.lang.String customerNumber, java.lang.String label,
-         java.lang.String type)
+         java.lang.String type, com.crossvale.oao.Balance balance,
+         java.lang.String branch_id,
+         com.crossvale.oao.Account_routing account_routing)
    {
       this.customerNumber = customerNumber;
       this.label = label;
       this.type = type;
+      this.balance = balance;
+      this.branch_id = branch_id;
+      this.account_routing = account_routing;
    }
 
 }
