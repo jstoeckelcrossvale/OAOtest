810fe309228db22879c2cdfffe613155c6adc928
kieserver <>
2018-07-27 15:40:55 -0500

    {/common/src/main/java/com/crossvale/common/Transaction.java}

--- common/src/main/java/com/crossvale/common/Transaction.java
+++ common/src/main/java/com/crossvale/common/Transaction.java
@@ -9,18 +9,10 @@ public class Transaction implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Label(value = "id")
+   @org.kie.api.definition.type.Label("id")
    private java.lang.String id;
-   @org.kie.api.definition.type.Label(value = "bank_routing")
-   private com.crossvale.common.AccountRouting bank_routing;
-   @org.kie.api.definition.type.Label(value = "account_routing")
-   private com.crossvale.common.AccountRouting account_routing;
-   @org.kie.api.definition.type.Label(value = "holder")
-   private com.crossvale.common.Holder holder;
-   @org.kie.api.definition.type.Label(value = "metadata")
-   private com.crossvale.common.AccountMetadata metadata;
-   @org.kie.api.definition.type.Label(value = "accountRoutingAsString")
-   private java.lang.String accountRoutingAsString;
+   @org.kie.api.definition.type.Label(value = "ticketNumber")
+   private java.lang.String ticketNumber;
 
    public Transaction()
    {
@@ -36,71 +28,20 @@ public class Transaction implements java.io.Serializable
       this.id = id;
    }
 
-   public com.crossvale.common.AccountRouting getBank_routing()
+   public java.lang.String getTicketNumber()
    {
-      return this.bank_routing;
+      return this.ticketNumber;
    }
 
-   public void setBank_routing(com.crossvale.common.AccountRouting bank_routing)
+   public void setTicketNumber(java.lang.String ticketNumber)
    {
-      this.bank_routing = bank_routing;
+      this.ticketNumber = ticketNumber;
    }
 
-   public com.crossvale.common.AccountRouting getAccount_routing()
-   {
-      return this.account_routing;
-   }
-
-   public void setAccount_routing(
-         com.crossvale.common.AccountRouting account_routing)
-   {
-      this.account_routing = account_routing;
-   }
-
-   public com.crossvale.common.Holder getHolder()
-   {
-      return this.holder;
-   }
-
-   public void setHolder(com.crossvale.common.Holder holder)
-   {
-      this.holder = holder;
-   }
-
-   public com.crossvale.common.AccountMetadata getMetadata()
-   {
-      return this.metadata;
-   }
-
-   public void setMetadata(com.crossvale.common.AccountMetadata metadata)
-   {
-      this.metadata = metadata;
-   }
-
-   public java.lang.String getAccountRoutingAsString()
-   {
-      return this.accountRoutingAsString;
-   }
-
-   public void setAccountRoutingAsString(
-         java.lang.String accountRoutingAsString)
-   {
-      this.accountRoutingAsString = accountRoutingAsString;
-   }
-
-   public Transaction(java.lang.String id,
-         com.crossvale.common.AccountRouting bank_routing,
-         com.crossvale.common.AccountRouting account_routing,
-         com.crossvale.common.Holder holder,
-         com.crossvale.common.AccountMetadata metadata,
-         java.lang.String accountRoutingAsString)
+   public Transaction(java.lang.String id, java.lang.String ticketNumber)
    {
       this.id = id;
-      this.bank_routing = bank_routing;
-      this.account_routing = account_routing;
-      this.holder = holder;
-      this.metadata = metadata;
-      this.accountRoutingAsString = accountRoutingAsString;
+      this.ticketNumber = ticketNumber;
    }
 
 }
